a57031ae4cd2bf10ddff1e708bb839ae883a7183
xSplit <rophp@live.it>
2016-01-10 16:25:33 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,13 @@
+<?php
+include 'simpleipn.php';
+
+if(SimpleIPN::isIPN()){
+    $ipn = new SimpleIPN();
+    $paypal = 'mymarket@email.com';
+    $amount = 10;
+    if($ipn->isCompleted($paypal,$amount)){
+        $id = $ipn->getID();
+        //STORE TRANSACTION
+        //DO STUFF
+    }
+}
