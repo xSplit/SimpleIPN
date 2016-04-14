1321d81994ace18dc076558d033194e7d71cf070
xSplit <xSplit@users.noreply.github.com>
2016-04-14 14:32:00 +0200

    Update example.php

--- example.php
+++ example.php
@@ -11,3 +11,17 @@ if(SimpleIPN::isIPN()){
         //DO STUFF $ipn->getCustom()
     }
 }
+
+include 'coinipn.php';
+
+if(CoinIPN::isIPN()){
+    $ipn = new CoinIPN();
+    $merchant_id = 'kj098as6bfbcvsd87sdnh75d9sgd789';
+    $ipn_secret = 'secretpass1337';
+    $amount = 10;
+    if($ipn->isVerified($merchant_id,$ipn_secret) && $ipn->isCompleted($amount)){
+        $id = $ipn->getID();
+        //STORE TRANSACTION
+        //DO STUFF $ipn->getCustom()
+    }
+}
