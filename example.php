728360b9e1e98411574d4c813d6901d700a6ada8
xSplit <xSplit@users.noreply.github.com>
2016-04-14 12:27:17 +0200

    Update example.php

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ if(SimpleIPN::isIPN()){
     $ipn = new SimpleIPN();
     $paypal = 'mymarket@email.com';
     $amount = 10;
-    if($ipn->isVerified() && $ipn->isCompleted($paypal,$amount)){
+    if($ipn->isVerified() && $ipn->receivedAt($paypal) && $ipn->isCompleted($amount)){
         $id = $ipn->getID();
         //STORE TRANSACTION
         //DO STUFF $_POST['custom']
