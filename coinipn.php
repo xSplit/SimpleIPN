99eeea6b5d1534786a052ffac3f2be7b3e4be7e3
xSplit <xSplit@users.noreply.github.com>
2016-04-15 11:44:12 +0200

    Update coinipn.php

--- coinipn.php
+++ coinipn.php
@@ -6,13 +6,13 @@ class CoinIPN{
         $this->req = file_get_contents('php://input');
     }
     
-    public function isVerified($merchant_id,$ipn_secret){
+    public function isVerified($merchant_id, $ipn_secret){
         return $this->req !== false && !empty($this->req) &&
             isset($_POST['merchant']) && $_POST['merchant'] == trim($merchant_id)
             && hash_hmac("sha512", $this->req, trim($ipn_secret)) == $_SERVER['HTTP_HMAC'];
     }
     
-    public function isCompleted($amount,$currency='USD'){
+    public function isCompleted($amount, $currency='USD'){
         return $amount >= floatval($_POST['amount1']) && $_POST['currency1'] == $currency
             && ($_POST['status'] >= 100 || $_POST['status'] == 2);
     }
