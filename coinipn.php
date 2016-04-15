404ac087ca8ac46bcde5b490def75131f435f98a
xSplit <xSplit@users.noreply.github.com>
2016-04-15 11:38:54 +0200

    Update coinipn.php

--- coinipn.php
+++ coinipn.php
@@ -1,17 +1,15 @@
 <?php
 class CoinIPN{
-
+	
     private $req;
     public function __construct(){
         $this->req = file_get_contents('php://input');
     }
-
     public function isVerified($merchant_id,$ipn_secret){
-        return !($this->req === FALSE || empty($this->req) ||
-            !isset($_POST['merchant']) || $_POST['merchant'] != trim($merchant_id)
-            || hash_hmac("sha512", $this->req, trim($ipn_secret)) != $_SERVER['HTTP_HMAC']);
+        return $this->req !== false && !empty($this->req) &&
+            isset($_POST['merchant']) && $_POST['merchant'] == trim($merchant_id)
+            && hash_hmac("sha512", $this->req, trim($ipn_secret)) == $_SERVER['HTTP_HMAC'];
     }
-
     public function isCompleted($amount,$currency='USD'){
         return $amount >= floatval($_POST['amount1']) && $_POST['currency1'] == $currency
             && ($_POST['status'] >= 100 || $_POST['status'] == 2);
@@ -24,13 +22,12 @@ class CoinIPN{
     public function getCustom(){
         return $_POST['custom'];
     }
-    
-    public function getItem($key){
+	
+	public function getItem($key){
         return $_POST['item_'.$key];
     }
-
+	
     public static function isIPN(){
-        return !(!isset($_POST['ipn_mode']) || $_POST['ipn_mode'] != 'hmac'
-            || !isset($_SERVER['HTTP_HMAC']) || empty($_SERVER['HTTP_HMAC']));
+        return isset($_POST['ipn_mode']) && $_POST['ipn_mode'] == 'hmac' && !empty($_SERVER['HTTP_HMAC']);
     }
 }
